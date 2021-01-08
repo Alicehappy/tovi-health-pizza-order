1e1238cb7a1f10749b637635a88b40eca3575338
ailibaobao <ailibaobao@ailibaobaosiMac.localdomain>
2021-01-08 14:43:45 -0500

    Added the slide left and right animations when switching between different views.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)', position: 'absolute', top: '0', width: '100%'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(-100%)'}))
+      ])
+    ]),
+    trigger('slideInOut1', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)', position: 'absolute', top: '0', width: '100%'}),
+        animate('200ms ease-out', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-out', style({transform: 'translateX(100%)'}))
+      ])
+    ]),
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'pizza-order';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
