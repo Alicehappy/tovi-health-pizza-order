e280061f03d6c7b10e40c84dee43096757deb8bd
ailibaobao <ailibaobao@ailibaobaosiMac.localdomain>
2021-01-08 10:59:49 -0500

    Completed basic Home view.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-
+  
+  eggRollNum: number;
+  pizzaNum: number;
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.eggRollNum = 3;
+    this.pizzaNum = 4;
   }
 
 }
