8db883969cb30fc8ac42b3c7bc140c16b509601a
ailibaobao <ailibaobao@ailibaobaosiMac.localdomain>
2021-01-08 11:39:57 -0500

    Rearchitecture the code and added *ngIf logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pizza-order';
+  eggRollNum: number;
+  pizzaNum: number;
+  newResult: boolean;
+  empResult: boolean;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.eggRollNum = 3;
+    this.pizzaNum = 4;
+    this.newResult = false;
+    this.empResult = false;
+  }
 }
