8b6e10db0722756ca70ff68dd6801c9881b95749
ailibaobao <ailibaobao@ailibaobaosiMac.localdomain>
2021-01-08 11:12:46 -0500

    Completed basic Results view: New result and empty result.

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
+  eggRollNum: number;
+  pizzaNum: number;
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.eggRollNum = 3;
+    this.pizzaNum = 4;
   }
 
 }
